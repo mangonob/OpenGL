156a67b698fdcf5cb48d497da85e015cb6b44b43
mangonob <534393806@qq.com>
2017-05-05 15:33:49 +0800

    +

--- main.cpp
+++ main.cpp
@@ -50,39 +50,6 @@ void init(void) {
 
     render_model_matrix_loc = glGetUniformLocation(program, "model_matrix");
     render_projection_matrix_loc = glGetUniformLocation(program, "projection_matrix");
-
-    object.LoadFromVBM("armadillo_low.vbm", 0, 1, 2);
-    object.BindVertexArray();
-
-    vec4 colors[INSTANCE_COUNT];
-
-    for (int n = 0; n < INSTANCE_COUNT; ++n) {
-        float a = float(n) / 4.0f;
-        float b = float(n) / 5.0f;
-        float c = float(n) / 6.0f;
-
-        colors[n][0] = 0.5f * (sinf(a + 1.0f) + 1.0f);
-        colors[n][1] = 0.5f * (sinf(b + 2.0f) + 1.0f);
-        colors[n][2] = 0.5f * (sinf(c + 3.0f) + 1.0f);
-        colors[n][3] = 1.0f;
-    }
-
-    glGenBuffers(2, VBOs);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[WeightBuffer]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), NULL, GL_DYNAMIC_DRAW);
-    glVertexAttribDivisor(3, 1);
-    glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-    glEnableVertexAttribArray(3);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[ColorBuffer]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-    glVertexAttribDivisor(4, 1);
-    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 0, NULL);
-    glEnableVertexAttribArray(4);
-
-    // Unbind object's array buffer
-    glBindVertexArray(0);
 }
 
 void display(void) {
@@ -94,65 +61,8 @@ void display(void) {
 
     float t = ((clock() / 10) & 0x1FFF) / float(0x1FFF);
 
-    vec4 weights[INSTANCE_COUNT];
-
-    for (int n = 0; n < INSTANCE_COUNT; ++n) {
-        float a = float(n) / 4.0f;
-        float b = float(n) / 5.0f;
-        float c = float(n) / 6.0f;
-
-        weights[n][0] = 0.5f * (sinf(t * 6.28318531f * 8.0f + a) + 1.0f);
-        weights[n][1] = 0.5f * (sinf(t * 6.28318531f * 26.0f + b) + 1.0f);
-        weights[n][2] = 0.5f * (sinf(t * 6.28318531f * 21.0f + c) + 1.0f);
-        weights[n][3] = 0.5f * (sinf(t * 6.28318531f * 13.0f + a + b) + 1.0f);
-    }
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[WeightBuffer]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(weights), weights, GL_DYNAMIC_DRAW);
-
-    // Clear
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Setup
-    glEnable(GL_CULL_FACE);
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-
-    mat4 model_matrix[4];
-
-    for (int n = 0; n < 4; ++n) {
-        model_matrix[n] = (vmath::scale(5.0f) *
-                           vmath::rotate(t * 360.0f * 40.0f + float(n + 1) * 29.0f, 0.0f, 1.0f, 0.0f) *
-                           vmath::rotate(t * 360.0f * 20.0f + float(n + 1) * 35.0f, 0.0f, 0.0f, 1.0f) *
-                           vmath::rotate(t * 360.0f * 30.0f + float(n + 1) * 67.0f, 0.0f, 1.0f, 0.0f) *
-                           vmath::translate((float)n * 10.0f - 15.0f, 0.0f, 0.0f) *
-                           vmath::scale(0.01f));
-    }
-
-    glUniformMatrix4fv(render_model_matrix_loc, 4, GL_FALSE, model_matrix[0]);
-
-    mat4 projection_matrix(frustum(-1.0f, 1.0f, -aspect, aspect, 1.0f, 5000.0f) * translate(0.0f, 0.0f, -100.0f));
-
-    glUniformMatrix4fv(render_projection_matrix_loc, 1, GL_FALSE, projection_matrix);
-
-    object.Render(0, 1);
 
     glFlush();
-}
-
-bool animated = true;
-
-void redisplay_timer(int v) {
-    static int a = 0;
-    if (animated)
-        glutTimerFunc((unsigned int)(1000 / 60), redisplay_timer, a);
-    glutPostRedisplay();
-    a++;
-}
-
-void clickEvent(int button, int state, int x, int y) {
-    animated = button != GLUT_LEFT_BUTTON;
-    if (animated) redisplay_timer(0);
 }
 
 int main(int argc, const char ** argv) {
@@ -165,8 +75,6 @@ int main(int argc, const char ** argv) {
     init();
 
     glutDisplayFunc(display);
-//    redisplay_timer(0);
-    glutMouseFunc(clickEvent);
 
     glutMainLoop();
 }
